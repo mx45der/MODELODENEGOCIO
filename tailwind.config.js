f395626f5107244773d4ade087fd130778228ccb
mx45der <mx69ccc@gmail.com>
2025-10-03 04:10:16 +0000

    css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./main.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
